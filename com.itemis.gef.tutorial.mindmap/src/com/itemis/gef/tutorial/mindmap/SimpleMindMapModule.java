29ce5b650e46f47c1d44401f8c8ffea6362e5097
mwienand <matthias.wienand@itemis.de>
2017-04-13 15:17:38 +0200

    Step 5: Add hover and selection feedback.

--- com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/SimpleMindMapModule.java
+++ com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/SimpleMindMapModule.java
@@ -3,6 +3,11 @@ package com.itemis.gef.tutorial.mindmap;
 import org.eclipse.gef.common.adapt.AdapterKey;
 import org.eclipse.gef.common.adapt.inject.AdapterMaps;
 import org.eclipse.gef.mvc.fx.MvcFxModule;
+import org.eclipse.gef.mvc.fx.handlers.FocusAndSelectOnClickHandler;
+import org.eclipse.gef.mvc.fx.handlers.HoverOnHoverHandler;
+import org.eclipse.gef.mvc.fx.parts.DefaultHoverFeedbackPartFactory;
+import org.eclipse.gef.mvc.fx.parts.DefaultSelectionFeedbackPartFactory;
+import org.eclipse.gef.mvc.fx.providers.ShapeBoundsProvider;
 import org.eclipse.gef.mvc.fx.providers.ShapeOutlineProvider;
 
 import com.google.inject.multibindings.MapBinder;
@@ -15,6 +20,19 @@ import com.itemis.gef.tutorial.mindmap.parts.SimpleMindMapAnchorProvider;
  *
  */
 public class SimpleMindMapModule extends MvcFxModule {
+
+	@Override
+	protected void bindAbstractContentPartAdapters(MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		super.bindAbstractContentPartAdapters(adapterMapBinder);
+
+		// binding the HoverOnHoverPolicy to every part
+		// if a mouse is moving above a part it is set i the HoverModel
+		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(HoverOnHoverHandler.class);
+
+		// add the focus and select policy to every part, listening to clicks
+		// and changing the focus and selection model
+		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(FocusAndSelectOnClickHandler.class);
+	}
 
 	@Override
 	protected void bindIContentPartFactoryAsContentViewerAdapter(MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
@@ -32,6 +50,14 @@ public class SimpleMindMapModule extends MvcFxModule {
 
 		// bind a geometry provider, which is used in our anchor provider
 		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(ShapeOutlineProvider.class);
+
+		// provides a hover feedback to the shape, used by the HoverBehavior
+		AdapterKey<?> role = AdapterKey.role(DefaultHoverFeedbackPartFactory.HOVER_FEEDBACK_GEOMETRY_PROVIDER);
+		adapterMapBinder.addBinding(role).to(ShapeOutlineProvider.class);
+
+		// provides a selection feedback to the shape
+		role = AdapterKey.role(DefaultSelectionFeedbackPartFactory.SELECTION_FEEDBACK_GEOMETRY_PROVIDER);
+		adapterMapBinder.addBinding(role).to(ShapeBoundsProvider.class);
 	}
 
 	@Override
